481d34326e315564ea3d7939e60686be9f587da6
subhankarghosh2000 <subhankar.github2000@gmail.com>
2021-09-10 19:07:19 +0530

    Add Assignment 1

--- Count Characters/count_characters_in_txt_file.py
+++ Count Characters/count_characters_in_txt_file.py
@@ -1,6 +1,8 @@
 file = open("passage.txt", "r")
 data = file.read()
 number_of_characters = len(data)
+number_of_words = data.split()
 print('Number of characters in text file :', number_of_characters)
+print('Number of words in text file :', len(number_of_words))
 
 
